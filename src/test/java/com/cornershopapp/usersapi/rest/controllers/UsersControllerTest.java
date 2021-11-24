dcb8a69e17c755c2806bdfc139b8816c29e46e49
Alejandro Sazo <alejandro.sazo@cornershopapp.com>
2021-11-24 19:50:44 -0300

    Adding more tests

--- src/test/java/com/cornershopapp/usersapi/rest/controllers/UsersControllerTest.java
+++ src/test/java/com/cornershopapp/usersapi/rest/controllers/UsersControllerTest.java
@@ -1,13 +1,16 @@
 package com.cornershopapp.usersapi.rest.controllers;
 
 import com.cornershopapp.usersapi.domain.dtos.UserDTO;
+import com.cornershopapp.usersapi.domain.records.CreateUserRequestRecord;
+import com.cornershopapp.usersapi.rest.request.users.CreateUserIBean;
 import com.cornershopapp.usersapi.services.UsersService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,9 +20,14 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -34,10 +42,13 @@ class UsersControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
     @DisplayName("GET /api/users empty list")
     void testGetUsersEmptyList() throws Exception {
-        Mockito.when(usersService.getAllUsers()).thenReturn(List.of());
+        when(usersService.getAllUsers()).thenReturn(List.of());
         mockMvc.perform(get("/api/users"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
@@ -50,7 +61,7 @@ class UsersControllerTest {
     @DisplayName("GET /api/users with one user")
     void testGetUsersWithOneUser() throws Exception {
         UUID uuid = UUID.randomUUID();
-        Mockito.when(usersService.getAllUsers()).thenReturn(
+        when(usersService.getAllUsers()).thenReturn(
                 List.of(
                         UserDTO.builder()
                                 .createdAt(Instant.now())
@@ -73,5 +84,66 @@ class UsersControllerTest {
                 .andExpect(jsonPath("$.users[0].email", is("jonsnow@cornershopapp.com")));
 
         verify(usersService, times(1)).getAllUsers();
+    }
+
+    @Test
+    @DisplayName("POST /api/users")
+    void testCreateUser() throws Exception {
+        CreateUserIBean payload = new CreateUserIBean();
+        payload.setEmail("sansa.stark@cornershopapp.com");
+        payload.setFirstName("Sansa");
+        payload.setLastName("Stark");
+
+        UUID uuid = UUID.randomUUID();
+        Instant now = Instant.now();
+        when(usersService.createUser(any(CreateUserRequestRecord.class))).thenReturn(
+                UserDTO.builder()
+                        .id(1L)
+                        .uuid(uuid)
+                        .lastName("Stark")
+                        .firstName("Sansa")
+                        .email("sansa.stark@cornershopapp.com")
+                        .updatedAt(now)
+                        .createdAt(now)
+                        .build()
+        );
+
+        mockMvc.perform(
+                post("/api/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(payload)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.email", is("sansa.stark@cornershopapp.com")))
+                .andExpect(jsonPath("$.first_name", is("Sansa")))
+                .andExpect(jsonPath("$.last_name", is("Stark")))
+                .andExpect(jsonPath("$.phone", nullValue()))
+                .andExpect(jsonPath("$.uuid", is(uuid.toString())));
+    }
+
+    @Test
+    @DisplayName("POST /api/users failed")
+    void testCouldNotCreateUserWhenAllPayloadFieldsAreNull() throws Exception {
+        CreateUserIBean payload = new CreateUserIBean();
+
+        mockMvc.perform(
+                        post("/api/users")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(asJsonString(payload)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.status_code", is(400)))
+                .andExpect(jsonPath("$.error", is("BAD_REQUEST")))
+                .andExpect(jsonPath("$.errors", hasSize(6)));
+
+        verify(usersService, never()).createUser(any(CreateUserRequestRecord.class));
+    }
+
+    private String asJsonString(final Object obj) {
+        try {
+            return objectMapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
